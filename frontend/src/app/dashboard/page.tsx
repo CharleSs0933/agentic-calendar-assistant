2c693e3b815da05d42c390481c0d3d4431b38a5a
Nguyễn Kiều Anh Quân <quansieuquay2013@gmail.com>
2026-09-11 16:07:20 +0700

    feat: Middleware implementation, user SQL schema, migration, and logout implementation

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useDescope } from "@descope/nextjs-sdk/client";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 function DashboardPage() {
-  return <div>DashboardPage</div>;
+  const sdk = useDescope();
+  const router = useRouter();
+  const [loggingOut, setLoggingOut] = useState(false);
+
+  async function handleLogout() {
+    if (loggingOut) return;
+    setLoggingOut(true);
+
+    try {
+      await sdk.logout();
+      router.replace("/sign-in");
+      router.refresh();
+    } catch (error) {
+      setLoggingOut(false);
+    }
+  }
+
+  return (
+    <div>
+      DashboardPage
+      <Button onClick={handleLogout}>Logout</Button>
+    </div>
+  );
 }
 
 export default DashboardPage;
